260a31c9abd6850c275d57498b46d4573fd83c63
Cooper Park <therealcooperpark@gmail.com>
2022-09-14 23:03:30 -0400

    Basic prediction algorithm, minus NaN management

--- README.md
+++ README.md
@@ -3,8 +3,18 @@
   This is my attempt at building a Machine Learning model to accurately predict the archetype of a weapon from Destiny 2 given its characteristics. Right now, this model will include:
   
   - Stats (e.g., handling, range, magazine size)
-  - Perk availability (e.g., Quickdraw, Rampage)
-  
+  - Element Type (e.g., Arc, Solar, Void, Stasis)
+  - Ammo type (e.g., Primary, Special, Heavy)
+
   All data is pulled from Destiny's API via the great work by [altbdoor](https://altbdoor.github.io/d2-api-human/)!
   
   This README will be updated as new features become available! Stay tuned!
+
+  ## Future goals
+
+  - Increase difficulty of algorithm predictions by removing Ammo type consideration
+  - Add perk pools to predictions to see if that alters prediction reliability
+  - Try to predict weapon frame, along with weapon type (or frame within a type)
+    - Can combine any of the other future goals with this one too
+  - Try to predict weapon rarity given stats (or anything else)
+    - Can combine any of the other future goals with this one too 
---
+++ prediction.py
@@ -0,0 +1,62 @@
+import joblib
+import json
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn import tree
+
+# Read database
+with open('data/weapons/all.json', 'r') as json_file:
+    js = json.load(json_file)
+df = pd.json_normalize(js)
+
+#####################
+# Clean up the data #
+#####################
+
+# 1) Remove sunset gear
+# (Assumes weapon type rules were adhered to better after sunsetting)
+df = df[df['powercap'].isna()]
+
+# 2) Keep only legendary gear
+# (Exotics are unique and under legendary likely has worse stats in general)
+# (Possibly worth experimenting as an alternate prediction though!)
+df = df[df['weapon_tier'] == 'legendary']
+
+# 3) Convert categorical columns into numerics
+ammo_dummies = pd.get_dummies(df['ammo_type'])
+element_dummies = pd.get_dummies(df['element_class'])
+df = pd.concat([df, ammo_dummies, element_dummies], axis=1)
+
+# 4) Drop non-stat columns
+df = df.drop(columns=['id', 'name', 'icon', 'watermark', 'screenshot',
+                      'flavor_text', 'weapon_tier', 'powercap', 'perks',
+                      'frame.name', 'frame.description', 'frame.icon',
+                      'ammo_type', 'element_class'])
+
+# 5) Process NaN values
+### TO DO
+
+
+###################
+# Build the model #
+###################
+
+# Create input and output
+X = df.drop(columns=['weapon_type'])
+y = df['weapon_type']
+
+# Create train/test datasets
+X_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# Build the model
+model = DecisionTreeClassifier()
+model.fit(X_train, y_train)
+
+# Test the model
+predictions = model.predict(X_test)
+
+# Measure accuracy
+accuracy = accuracy_score(y_test, predictions)
+print('Accuracy: {0}'.format(accuracy))
